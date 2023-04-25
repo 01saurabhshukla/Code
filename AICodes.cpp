d76f4fced1cca1e45638a919258948c28858dc4d
Saurabh Shukla <88873969+01saurabhshukla@users.noreply.github.com>
2023-04-25 22:46:03 +0530

    Update AICodes.cpp

--- AICodes.cpp
+++ AICodes.cpp
@@ -157,79 +157,178 @@ int main()
 
 
 
+Bayesian Network
 
-A* Code
-def aStarAlgo(start_node, stop_node):
-open_set = set(start_node)
-closed_set = set()
-g = {}
-parents = {}
-g[start_node] = 0
-parents[start_node] = start_node
-while len(open_set) > 0:
-n = None
-for v in open_set:
-if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
-n = v
-if n == stop_node or Graph_nodes[n] == None:
-pass
-else:
-for (m, weight) in get_neighbors(n):
-if m not in open_set and m not in closed_set:
-open_set.add(m)
-parents[m] = n
-g[m] = g[n] + weight
-else:
-if g[m] > g[n] + weight:
-g[m] = g[n] + weight
-parents[m] = n
-if m in closed_set:
-closed_set.remove(m)
-open_set.add(m)
-if n == None:
-print('Path does not exist!')
-return None
-if n == stop_node:
-path = []
-while parents[n] != n:
-path.append(n)
-n = parents[n]
-path.append(start_node)
-path.reverse()
-print('Path found: {}'.format(path))
-return path
-open_set.remove(n)
-closed_set.add(n)
-print('Path does not exist!')
-return None
-def get_neighbors(v):
-if v in Graph_nodes:
-return Graph_nodes[v]
-else:
-return None
-def heuristic(n):
-H_dist = {
-'A': 11,
-'B': 6,
-'C': 5,
-'D': 7,
-'E': 3,
-'F': 6,
-'G': 5,
-'H': 3,
-'I': 1,
-'J': 0
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Node {
+public:
+    vector<double> values;
+};
+
+class VariableNode : public Node {
+public:
+    int numValues;
+};
+
+class CPT {
+public:
+    vector<double> probabilities;
+};
+
+class BayesianNetwork {
+public:
+    VariableNode A, B;
+    CPT cptA, cptB;
+
+
+    double query(int a, int b) {
+        double result = 0.0;
+        for (int i = 0; i < A.numValues; i++) {
+            for (int j = 0; j < B.numValues; j++) {
+                result += cptA.probabilities[i] * cptB.probabilities[j] * (a == i) * (b == j);
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    BayesianNetwork net;
+    net.A.numValues = 2;
+    net.B.numValues = 2;
+    net.A.values = {0.3, 0.7};
+    net.B.values = {0.6, 0.4};
+    net.cptA.probabilities = {0.7, 0.3};
+    net.cptB.probabilities = {0.4, 0.6};
+
+    double q = net.query(0, 1);
+    cout << "P(A=0, B=1) = " << q << endl;
+    return 0;
 }
-return H_dist[n]
-Graph_nodes = {
-'A': [('B', 6), ('F', 3)],
-'B': [('A', 6), ('C', 3), ('D', 2)],
-'C': [('B', 3), ('D', 1), ('E', 5)],
-'D': [('B', 2), ('C', 1), ('E', 8)],
-'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-'F': [('A', 3), ('G', 1), ('H', 7)],
-'G': [('F', 1), ('I', 3)],
-'H': [('F', 7), ('I', 2)],
-'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
+
+
+
+A* Search
+
+from collections import deque
+
+class Graph:
+    # example of adjacency list (or rather map)
+    # adjacency_list = {
+    # 'A': [('B', 1), ('C', 3), ('D', 7)],
+    # 'B': [('D', 5)],
+    # 'C': [('D', 12)]
+    # }
+
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
+
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
+
+    # heuristic function with equal values for all nodes
+    def h(self, n):
+        H = {
+            'A': 1,
+            'B': 1,
+            'C': 1,
+            'D': 1
+        }
+
+        return H[n]
+
+    def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v;
+
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+
+                reconst_path.reverse()
+
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+
+            # for all neighbors of the current node do
+            for (m, weight) in self.get_neighbors(n):
+                # if the current node isn't in both open_list and closed_list
+                # add it to open_list and note n as it's parent
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update parent data and g data
+                # and if the node was in the closed_list, move it to open_list
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        return None
+        
+adjacency_list = {
+    'A': [('B', 1), ('C', 3), ('D', 7)],
+    'B': [('D', 5)],
+    'C': [('D', 12)]
 }
-aStarAlgo('A', 'J')
+graph1 = Graph(adjacency_list)
+graph1.a_star_algorithm('A', 'D')
+  
+  
+  
+Output:
+Path found: ['A', 'B', 'D']
+['A', 'B', 'D']
+
+Thus, the optimal path from A to D, found using A*, is A->B->D.
